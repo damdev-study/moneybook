716eee7a22023ba18210abefc2a722addc97ef12
shapl <josh@shapl.com>
2018-11-24 15:10:50 +0900

    #1 기본파일만 등록

---
+++ src/main/java/api/damdev/moneybook/domain/UserInfo.java
@@ -0,0 +1,4 @@
+package api.damdev.moneybook.domain;
+
+public class UserInfo {
+}
---
+++ src/main/java/api/damdev/moneybook/service/impl/UserServiceImpl.java
@@ -0,0 +1,7 @@
+package api.damdev.moneybook.service.impl;
+
+import api.damdev.moneybook.service.UserService;
+
+public class UserServiceImpl implements UserService {
+
+}
