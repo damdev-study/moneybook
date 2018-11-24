fdfdbc3835efff4f78bbd289cca02f0921653501
shapl <josh@shapl.com>
2018-11-24 14:36:13 +0900

    first commit

---
+++ src/main/java/api/damdev/moneybook/MoneybookApplication.java
@@ -0,0 +1,12 @@
+package api.damdev.moneybook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneybookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoneybookApplication.class, args);
+    }
+}
