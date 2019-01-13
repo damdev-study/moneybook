a5b7003face8409cea3668056da170a35237c66b
Jeong Eunwoo <zenic88@naver.com>
2019-01-13 22:38:19 +0900

    #37 도메인, DTO 테스트 코드 추가

--- src/test/java/api/damdev/moneybook/dto/MoneyInfoTest.java
+++ src/test/java/api/damdev/moneybook/dto/MoneyInfoTest.java
@@ -1,9 +1,31 @@
 package api.damdev.moneybook.dto;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Test;
+
 /**
  * Author : zenic
  * Created : 2019-01-13
  */
 public class MoneyInfoTest {
 
+  @Test
+  public void builder() {
+    MoneyInfo history = MoneyInfo.builder()
+      .money("10000")
+      .build();
+
+    assertThat(history.getUserSeqId()).isNull();
+    assertThat(history.getMoney()).isNotNull();
+  }
+
+  @Test
+  public void javaBean() {
+    MoneyInfo history = new MoneyInfo();
+    history.setMoney("10000");
+
+    assertThat(history.getUserSeqId()).isNull();
+    assertThat(history.getMoney()).isNotNull();
+  }
 }
